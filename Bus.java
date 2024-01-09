d6a147cdd731ded9f9aa6118d66bce43306a0919
athalagibelajar <141887252+athalagibelajar@users.noreply.github.com>
2024-01-09 19:50:59 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,10 @@
+package PBO.Latihan;
+
+public class Bus {
+    public int penumpang,maxpenumpang;
+
+    public void cetak(){
+        System.out.println("Penumpang sekarang = "+penumpang);
+        System.out.println("Penumpang seharusnya adalah ="+maxpenumpang);
+    }
+}
